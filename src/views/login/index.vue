ef79103e58e641dd57439b33f75fb6a58f1e5125
zphtml <zphtml@163.com>
2017-05-15 09:07:24 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,35 +1,63 @@
 <template>
-    <div class="login-container">
-        <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left"
-                 label-width="0px"
-                 class="card-box login-form">
-            <h3 class="title">系统登录</h3>
-            <el-form-item prop="email">
-                <span class="svg-container"><wscn-icon-svg icon-class="jiedianyoujian"/></span>
-                <el-input name="email" type="text" v-model="loginForm.email" autoComplete="on"
-                          placeholder="邮箱"></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-                <span class="svg-container"><wscn-icon-svg icon-class="mima"/></span>
-                <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password"
-                          autoComplete="on" placeholder="密码"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
-                    登录
-                </el-button>
-            </el-form-item>
-            <div class='tips'>admin账号为:admin@wallstreetcn.com 密码随便填</div>
-            <div class='tips'>editor账号:editor@wallstreetcn.com 密码随便填</div>
-            <router-link to="/sendpwd" class="forget-pwd">
-                忘记密码?(或首次登录)
-            </router-link>
-        </el-form>
-        <el-dialog title="第三方验证" v-model="showDialog">
-            邮箱登录成功,请选择第三方验证
-            <socialSign></socialSign>
-        </el-dialog>
-    </div>
+<el-row style="height: 100%;width: 100%; position: absolute;" class="login">
+    <el-row style="margin-top:10%;">
+         <el-col :span="24" style="text-align: center">
+             <img src="http://test.yushan.mobi//yushan_vue/羽扇.png" alt="" style="width: 300px;">
+         </el-col>
+    </el-row>
+    <el-row  style="position: absolute;width: 100%;left: 0;bottom: 15%;">
+         <el-col :span="24">
+             <el-col :span="12" style="text-align: right;padding-right: 30px;">
+                 <img src="http://test.yushan.mobi//yushan_vue/组-6.png" alt="" style="width: 350px;margin-top:50px;">
+              </el-col>
+             <el-col :span="12" style="padding-left: 30px;">
+                 <div class="login1">
+
+                     <el-col :span="24" class="denglu">
+                         <img src="http://test.yushan.mobi//yushan_vue/矩形-10.png" alt="" class="rent">
+                         <span>用户登录</span>
+                     </el-col>
+                     <el-form id="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm">
+                         <el-form-item prop="email">
+                            <el-col :span="24" class="el-input" style="margin-top: 20px;">
+                                 <el-input type="text" name="username"  v-model="loginForm.email" autoComplete="on" class="username" placeholder="登录名/邮箱"/>
+                            </el-col>
+                         </el-form-item>
+                         <el-form-item prop="password">
+                         <el-col :span="24" style="margin-top: 10px;">
+                            <el-input type="password" id="showPwd" class="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" placeholder="请输入密码" />
+                         </el-col>
+                         </el-form-item>
+                          <el-col :span="24" style="margin-top: 20px;">
+                             <el-col :span="12" style="padding-left: 0;">
+                                 <el-checkbox v-model="loginForm.checked">自动登录</el-checkbox>
+                                 <!--<label>-->
+                                     <!--<input type="checkbox" id="check" style="vertical-align: middle;margin-top: 0;">-->
+                                     <!--<span for="check" style="color: #cacfd7;vertical-align: middle;font-size:12px;">自动登录</span>-->
+                                 <!--</label>-->
+
+                             </el-col>
+                              <el-col :span="12" style="text-align: right;color: #EC5A55;padding-right: 0;font-size: 12px">
+                                 <label>忘记密码 ？</label>
+                             </el-col>
+                        </el-col>
+                        <el-col :span="24" style="margin-top:40px;">
+                            <el-input type="submit" class="load" :loading="loading" @click.native.prevent="handleLogin"   value="登录"/>
+                          </el-col>
+                         <el-col :span="24" style="margin-top: 10px;">
+                             <el-input type="button" class="shenqing" value="申请使用"/>
+                         </el-col>
+                         </el-form>
+                 </div>
+              </el-col>
+        </el-col>
+     </el-row>
+       <el-row class=" banquan" style="position:absolute;bottom: 30px;width: 100%;color: #fff;font-size: 14px;">
+         <el-col :span="24" style="text-align: center;">
+             京ICP备15010702号-1 2011-2016 &copy; 北京智投智道投资咨询有限公司版权所有
+         </el-col>
+    </el-row>
+</el-row>
 </template>
 
 <script>
@@ -39,155 +67,209 @@
     import socialSign from './socialsignin';
 
     export default {
-      components: { socialSign },
-      name: 'login',
-      data() {
-        const validateEmail = (rule, value, callback) => {
-          if (!isWscnEmail(value)) {
-            callback(new Error('请输入正确的合法邮箱'));
-          } else {
-            callback();
-          }
-        };
-        const validatePass = (rule, value, callback) => {
-          if (value.length < 6) {
-            callback(new Error('密码不能小于6位'));
-          } else {
-            callback();
-          }
-        };
-        return {
-          loginForm: {
-            email: 'admin@wallstreetcn.com',
-            password: ''
-          },
-          loginRules: {
-            email: [
-                { required: true, trigger: 'blur', validator: validateEmail }
-            ],
-            password: [
-                { required: true, trigger: 'blur', validator: validatePass }
-            ]
-          },
-          loading: false,
-          showDialog: false
-        }
-      },
-      computed: {
-        ...mapGetters([
-          'auth_type'
-        ])
-      },
-      methods: {
-        handleLogin() {
-          this.$refs.loginForm.validate(valid => {
-            if (valid) {
-              this.loading = true;
-              this.$store.dispatch('LoginByEmail', this.loginForm).then(() => {
-                this.loading = false;
-                this.$router.push({ path: '/dashboard' });
-                // this.showDialog = true;
-              }).catch(err => {
-                this.$message.error(err);
-                this.loading = false;
-              });
-            } else {
-              console.log('error submit!!');
-              return false;
+        components: { socialSign },
+        name: 'login',
+        data() {
+            const validateEmail = (rule, value, callback) => {
+                if (!isWscnEmail(value)) {
+                    callback(new Error('请输入正确的合法邮箱'));
+                } else {
+                    callback();
+                }
+            };
+            const validatePass = (rule, value, callback) => {
+                if (value.length < 6) {
+                    callback(new Error('密码不能小于6位'));
+                } else {
+                    callback();
+                }
+            };
+            return {
+                loginForm: {
+                    email: 'admin@wallstreetcn.com',
+                    password: ''
+                },
+                loginRules: {
+                    email: [
+                        { required: true, trigger: 'blur', validator: validateEmail }
+                    ],
+                    password: [
+                        { required: true, trigger: 'blur', validator: validatePass }
+                    ]
+                },
+                loading: false,
+                showDialog: false
             }
-          });
+        },
+        computed: {
+                ...mapGetters([
+                    'auth_type'
+                ])
+    },
+    methods: {
+        handleLogin() {
+            this.$refs.loginForm.validate(valid => {
+                if (valid) {
+                    this.loading = true;
+                    this.$store.dispatch('LoginByEmail', this.loginForm).then(() => {
+                        this.loading = false;
+                    this.$router.push({ path: '/reports/overview' });
+                    // this.showDialog = true;
+                }).catch(err => {
+                        this.$message.error(err);
+                    this.loading = false;
+                });
+                } else {
+                    console.log('error submit!!');
+            return false;
+        }
+        });
         },
         afterQRScan() {
-          // const hash = window.location.hash.slice(1);
-          // const hashObj = getQueryObject(hash);
-          // const originUrl = window.location.origin;
-          // history.replaceState({}, '', originUrl);
-          // const codeMap = {
-          //   wechat: 'code',
-          //   tencent: 'code'
-          // };
-          // const codeName = hashObj[codeMap[this.auth_type]];
-          // if (!codeName) {
-          //   alert('第三方登录失败');
-          // } else {
-          //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-          //     this.$router.push({ path: '/' });
-          //   });
-          // }
+            // const hash = window.location.hash.slice(1);
+            // const hashObj = getQueryObject(hash);
+            // const originUrl = window.location.origin;
+            // history.replaceState({}, '', originUrl);
+            // const codeMap = {
+            //   wechat: 'code',
+            //   tencent: 'code'
+            // };
+            // const codeName = hashObj[codeMap[this.auth_type]];
+            // if (!codeName) {
+            //   alert('第三方登录失败');
+            // } else {
+            //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
+            //     this.$router.push({ path: '/' });
+            //   });
+            // }
         }
-      },
-      created() {
+    },
+    created() {
         // window.addEventListener('hashchange', this.afterQRScan);
-      },
-      destroyed() {
+    },
+    destroyed() {
         // window.removeEventListener('hashchange', this.afterQRScan);
-      }
+    }
     }
 </script>
-
 <style rel="stylesheet/scss" lang="scss">
     @import "src/styles/mixin.scss";
-    .tips{
-      font-size: 14px;
-      color: #fff;
-      margin-bottom: 5px;
+
+    .login{
+    width:100%;
+    height:100%;
+    background: url("http://test.yushan.mobi//yushan_vue/羽扇2_01.png") no-repeat;
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+    min-height:730px ;
+    position: relative;
+    overflow:hidden;
+}
+.user,.pwd{
+    margin-left: 20px;
+    border: 0;
+    /*border-bottom: 1px solid rgb(0,175,233);*/
+    border-bottom: 1px solid rgb(153,153,153);
+}
+.login1{
+    width: 350px;
+    height: 370px;
+    border-radius: 5px;
+    background: #fff;
+    box-shadow: 0px 0px 6px #d9d9d9;
+    font-family: "Microsoft Yahei";
+    padding: 0 15px;
+}
+.denglu{
+    line-height: 40px;
+    height: 40px;
+    font-size: 15px;
+    font-weight: 700;
+    margin-top: 30px;
+    position: relative;
+}
+#wrapper {
+    width: 100%;
+    height:100%;
+}
+.rent{
+    width: 7px;
+    position: absolute;
+    font-family: "Microsoft Yahei";
+    left: -15px;
+    line-height: 40px;
+    height: 40px;
+    top: 0;
+}
+.username,.password{
+    width:100%;
+    border-radius: 3px;
+    height:40px;
+    line-height:40px;
+    /*margin-top: 30px;*/
+}
+.el-input__inner {
+    height:40px!important;
+    line-height: 35px!important;
+}
+    .el-form-item {
+        margin-bottom: 0 !important;;
     }
-    .login-container {
-        @include relative;
-        height: 100vh;
-        background-color: #2d3a4b;
-
-        input:-webkit-autofill {
-            -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
-            -webkit-text-fill-color: #fff !important;
-        }
-        input {
-            background: transparent;
-            border: 0px;
-            -webkit-appearance: none;
-            border-radius: 0px;
-            padding: 12px 5px 12px 15px;
-            color: #eeeeee;
-            height: 47px;
-        }
-        .el-input {
-            display: inline-block;
-            height: 47px;
-            width: 85%;
-        }
-        .svg-container {
-            padding: 6px 5px 6px 15px;
-            color: #889aa4;
-        }
-
-        .title {
-            font-size: 26px;
-            font-weight: 400;
-            color: #eeeeee;
-            margin: 0px auto 40px auto;
-            text-align: center;
-            font-weight: bold;
-        }
-
-        .login-form {
-            position: absolute;
-            left: 0;
-            right: 0;
-            width: 400px;
-            padding: 35px 35px 15px 35px;
-            margin: 120px auto;
-        }
-
-        .el-form-item {
-            border: 1px solid rgba(255, 255, 255, 0.1);
-            background: rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            color: #454545;
-        }
-
-        .forget-pwd {
-            color: #fff;
-        }
+.load input{
+    font-weight: 600;
+    font-size: 15px !important;
+    letter-spacing: 20px;
+    background: -webkit-linear-gradient(#01b8ee, #3299e5); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(#01b8ee, #3299e5); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(#01b8ee, #3299e5); /* Firefox 3.6 - 15 */
+    background: linear-gradient(#01b8ee, #3299e5); /* 标准的语法 */
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    color: #fff;
+    border: 0;
+    border-radius: 3px;
+}
+.el-checkbox__inner {
+    width: 14px!important;
+    height: 14px!important;
+}
+.el-checkbox__inner::after {
+    height: 7px!important;
+    left: 3px!important;
+}
+.el-checkbox__label {
+        font-size: 12px!important;
     }
+.shenqing input{
+    font-weight: 600;
+    font-size: 15px !important;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    color: #4cbefe;
+    background: #fff;
+    border:1px solid #3299e5;
+    border-radius: 3px;
+}
+/*@media screen and (max-width:1000px) {
+   .banquan{
+       display: none;
+   }
+
+}
+@media screen and (max-height:700px) {
+    .banquan{
+        display: none;
+    }
+
+}*/
+input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px white inset !important;
+    -webkit-text-fill-color: #333;
+}
 
 </style>
--- src/views/reports/overview.vue
+++ src/views/reports/overview.vue
@@ -1,7 +1,241 @@
 <template>
-    <div class="components-container">
-        <code>这里暂时列出了自己在项目中自己封装和用到的组件，如有补充可以提<a href='https://github.com/PanJiaChen/vue-element-admin/issues' target='_blank'>issue</a><br/>
-            我个人崇尚自己封装组件，因为很多组件会和业务后高度的耦合，很多时候第三方封装是满足不了需求的，如有需要可以看楼主之前写过的一篇<a href='https://segmentfault.com/a/1190000009090836' target='_blank'>文章</a>
-        </code>
-    </div>
+        <el-row>
+            <el-col :span="24" class=" gailan">
+                <el-col :span="24" class=" gailan-toubiao">
+                        概览
+                </el-col>
+                <el-col :span="24" class=" gailan-zhi">
+                    <el-col :span="6" class="gailan-xiaofei">
+                            <div style="border-right: 1px solid #e2e4e7;">
+                                <div style="font-size: 14px;color: #3b4e61;">
+                                    本月消费
+                                </div>
+                                <div class="month" style="font-size: 16px;color: #00b7ee;font-family: 500;margin-top: 10px;">
+                                    Loading...
+                                </div>
+                            </div>
+                    </el-col>
+                    <el-col :span="6" class=" gailan-xiaofei">
+                            <div style="border-right: 1px solid #e2e4e7;">
+                                <div style="font-size: 14px;color: #3b4e61;">
+                                    今日消费
+                                </div>
+                                <div class="today" style="font-size: 16px;color: #00b7ee;font-family: 500;margin-top: 10px;">
+                                    Loading...
+                                </div>
+                            </div>
+                       </el-col>
+                    <el-col :span="6" class=" gailan-xiaofei">
+                            <div style="border-right: 1px solid #e2e4e7;">
+                                <div style="font-size: 14px;color: #3b4e61;">
+                                    昨日消费
+                                </div>
+                                <div class="yesterday" style="font-size: 16px;color: #00b7ee;font-family: 500;margin-top: 10px;">
+                                    Loading...
+                                </div>
+                            </div>
+                    </el-col>
+                    <el-col :span="6" class=" gailan-xiaofei" style="position: relative;">
+                            <div style="border-right: 1px solid #e2e4e7;">
+                                <div style="font-size: 14px;color: #3b4e61;">
+                                    账户余额
+                                    <!-- <a href="#" class="tooltip-show" data-toggle="tooltip"
+                                        title="默认的 Tooltip">-->
+                                    <i class="iconfont wen" style="cursor: pointer;">&#xe767;</i>
+                                    <!--</a>-->
+                                </div>
+                                <div class="apie1" style="font-size: 16px;color: #00b7ee;font-family: 500;margin-top: 10px;">
+                                    Loading...
+                                </div>
+                            </div>
+                            <div class="jinggao">
+                                您的资源仅供
+                                <span style="margin-left:2px;margin-right:2px;font-weight: 500;font-size: 16px;" class="ziyuan">0</span>
+                                天消费请尽快消费！
+                            </div>
+                    </el-col>
+                </el-col>
+             </el-col>
+            <el-col :span="24"  style="padding: 0;margin-top: 30px;">
+                <el-col :span="14" class=" duibi">
+                    <el-col :span="24" class=" contin">
+                        <el-col :span="24" class=" gailan-toubiao">
+                                对比率
+                            </el-col>
+                        <el-col :span="24" class=" duibi-pie">
+                            <el-col :span="8"  id="pie" style="height:450px;padding: 5px;">
+
+                                </el-col>
+                            <el-col :span="8"  id="pie1" style="height:450px;padding: 5px;">
+
+                                </el-col>
+                            <el-col :span="8"  id="pie2" style="height:450px;padding: 5px;">
+
+                                </el-col>
+                            </el-col>
+                       </el-col>
+                </el-col>
+                <el-col :span="10" class=" zhuanhua">
+                    <el-col :span="24" class=" contin">
+                        <el-col :span="24" class=" gailan-toubiao">
+                                转化漏斗
+                            </el-col>
+                            <el-col :span="24" class=" duibi-pie">
+                                <el-col :span="24" id="funnel" style="height:450px;padding: 0px;">
+
+                               </el-col>
+                            </el-col>
+                        </el-col>
+                 </el-col>
+
+                </el-col>
+            <el-col :span="24" class=" zhexian" style="padding: 0;margin-top: 30px;margin-bottom: 30px;">
+                <el-col :span="24" class=" gailan-toubiao">
+                        <!--<div class="dropdown">-->
+                            <!--<button type="button" class="btn1 dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" style="border: 0;background: #fafbfc;">近两周总体数据-->
+                                <!--<span class="caret"></span>-->
+                            <!--</button>-->
+                            <!--<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">-->
+                                <!--<li role="presentation">-->
+                                    <!--<a role="menuitem" tabindex="-1" href="javascript:void(0)" class="near_twoweek">近两周总体数据</a>-->
+                                <!--</li>-->
+                                <!--<li role="presentation">-->
+                                    <!--<a role="menuitem" tabindex="-1" href="javascript:void(0)" class="near_week">每周总消费趋势</a>-->
+                                <!--</li>-->
+                            <!--</ul>-->
+                        <!--</div>-->
+                    <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                        </el-option>
+                    </el-select>
+                   </el-col>
+                <el-col :span="24" style="background: #fff;">
+                    <el-col :span="24"  class="" id="mainn" style="height:500px;background: #fff;"></el-col>
+                    </el-col>
+
+               </el-col>
+           </el-row>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                options: [{
+                    value: '选项1',
+                    label: '近两周总体数据'
+                }, {
+                    value: '选项2',
+                    label: '每周总消费趋势'
+                }],
+                value: ''
+            }
+        }
+    }
+</script>
+<style rel="stylesheet/scss" lang="scss">
+    @import "src/styles/mixin.scss";
+    @import "src/styles/element-ui.scss";
+    @import "src/styles/rest.scss";
+    html{
+        background:#f5f7f9;
+    }
+    #page-wrapper{
+        background:#f5f7f9;
+        padding-left: 30px;
+        padding-right: 30px;
+    }
+    .iconfont{
+        font-family:"iconfont" !important;
+        font-size:16px;font-style:normal;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-stroke-width: 0.2px;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    .alert{
+        background: #fff;
+        border-radius: 0;
+        border-left: 5px solid #EC5A55;
+        box-shadow: 0px 0px 6px #d9d9d9;
+        font-family: "Microsoft Yahei";
+        font-size: 16px;
+        color: #3b4e61;
+    }
+    .zhexian{
+        box-shadow: 0px 0px 6px #d9d9d9;
+    }
+    .gailan{
+        padding: 0;
+        box-shadow: 0px 0px 6px #d9d9d9;
+    }
+    .duibi{
+        padding-left: 0;
+        padding-right: 10px;
+    }
+    .zhuanhua{
+        padding-right: 0;
+        padding-left: 10px;
+    }
+    .duibi .contin,.zhuanhua .contin{
+        box-shadow: 0px 0px 6px #d9d9d9;
+        padding: 0;
+    }
+    .duibi-pie{
+        padding: 0;
+        height: 450px;
+        background: #fff;
+        -webkit-box-orient:horizontal;
+        -webkit-box-pack: center;
+        display: -webkit-box;
+    }
+    .gailan-toubiao{
+        border-bottom: 1px solid #ebebeb;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        background: #fafbfc;
+        font-size: 14px;
+        color: #3b4161;
+        font-family: 'Microsoft YaHei';
+    }
+    .gailan-zhi{
+        padding:0;
+        height: 120px;
+        -webkit-box-orient:horizontal;
+        -webkit-box-pack: center;
+        display: -webkit-box;
+        background: #fff;
+    }
+    .gailan-xiaofei{
+        padding: 0;
+        height: 120px;
+        -webkit-box-orient:  vertical;
+        -webkit-box-pack: center;
+        display: -webkit-box;
+        text-align: center;
+        font-family: Microsoft YaHei, sans-serif, Arial;
+    }
+    .jinggao{
+        display: none;
+        position: absolute;
+        right: 10px;
+        top:-35px;
+        color: #fff;
+        width: 160px;
+        text-align: center;
+        padding: 10px 0;
+        border-radius: 5px;
+        font-family: "Microsoft Yahei";
+        font-size: 14px;
+        background: #ec5a55;
+    }
+    #mainn{
+        background: #fff;
+    }
+
+
+</style>
+
+
